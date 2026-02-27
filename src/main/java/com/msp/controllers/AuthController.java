e7fe76771c947503e3c6f45dde8f95ebd1d9f515
P-Odiix <mahingarodin@gmail.com>
2026-02-27 06:09:09 +0200

    [fix: fixed the auth_apis_request_mapper (/api/auth)]

--- src/main/java/com/msp/controllers/AuthController.java
+++ src/main/java/com/msp/controllers/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication", description = "Authentication management APIs for user signup and login")
 public class AuthController {
